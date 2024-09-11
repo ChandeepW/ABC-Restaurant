013e1aa120fb3ff0f7a57873382a80c1ecb4e886
Chandeep Wasala <chandeepw@icloud.com>
2024-09-11 11:33:34 +0530

    added script

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+// import { getdatabase,ref,set,get,child} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+
+
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
+
+//   // Your web app's Firebase configuration
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyCXIwq80fFA_UEC9lUp8eogdy0gm7uEMR8",
+//     authDomain: "abc-rest.firebaseapp.com",
+//     projectId: "abc-rest",
+//     storageBucket: "abc-rest.appspot.com",
+//     messagingSenderId: "606772076544",
+//     appId: "1:606772076544:web:25a6e1ce3fd9db773bb8e5"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+
+//   const db = getDatabase(app);
+
+//   document.getElementById("bookingForm").addEventListener('click', function(e)){
+
+//     set(ref(db, 'user/' + document.getElementById("name").value),
+//     {
+//         username: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         date: document.getElementById("datetime"),
+//         select: document.getElementById("select1"),
+//         extra: document.getElementById("message")
+//     })
+
+//   }
+
+  
